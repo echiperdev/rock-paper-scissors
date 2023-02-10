d9b0965d6e5c0f102f8bc77103375f37dfc2bcef
emilianchiper <123456789+echiperdev.noreply.github.com>
2023-02-10 13:40:46 +0200

    Store computer player choice
    Created function to store array item randomly selected by computer player.

--- play.js
+++ play.js
@@ -2,6 +2,10 @@
 let playerScore = 0;
 let compScore = 0;
 
-// Stores choice for computer player
+// Presents weapon options for computer player
 const weaponSelection = ["rock", "paper", "scissors"]; // Array of three possible choices
 
+// Stores randomly selected computer player choice
+function getCompChoice() {
+    return weaponSelection [Math.floor(Math.random() * 3)]; // Calls 'weaponSelection' constant and returns random item from stored array
+}
